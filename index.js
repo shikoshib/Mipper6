c01d38692e70d8d6da49489c605ac4960d8edcfb
shikoshib <shikoshib@yandex.ru>
2022-03-16 09:49:22 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express()
-const config = require('./config.json');
 
 const Discord = require("discord.js");
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"], allowedMentions: ["users"]});
