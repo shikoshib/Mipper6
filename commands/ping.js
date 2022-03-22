e4455e2b11837100cd33207bff14cf0d7ada7d57
shikoshib <shikoshib@yandex.ru>
2022-03-22 10:14:58 +1000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+    let guildLanguages = require("./guilds-language.json");
+    const guildLanguage = guildLanguages[message.guild.id] || "en"; 
+    const language = require(`./languages/${guildLanguage}`);
   let embedP = new Discord.MessageEmbed()
-        .setTitle(`Задержка составляет ${Date.now() - message.createdTimestamp} мс.`)
-            .setAuthor(`Запросил ${message.author.username}`, message.author.displayAvatarURL())
+        .setTitle(`${language("LATENCY")} ${Date.now() - message.createdTimestamp} ${language("MS")}.`)
+            .setAuthor(`${language("REQ")} ${message.author.username}`, message.author.displayAvatarURL())
 .setColor("#807fff")
-    .setFooter("Mipper6, 2022. Все права отсутствуют.", "https://mipper6.cf/resources/Mipper6.png")
+    .setFooter(language("RIGHTS"), "https://mipper6.cf/resources/Mipper6.png")
       message.channel.send({embeds:[embedP]});
 }
 
