3fb9cc6fa1cb1b9160f59255a6386aec24eccf1d
shikoshib <shikoshib@yandex.ru>
2022-03-22 10:09:42 +1000

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,14 +1,17 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+    let guildLanguages = require("./guilds-language.json");
+    const guildLanguage = guildLanguages[message.guild.id] || "en"; 
+    const language = require(`./languages/${guildLanguage}`);
   let user = message.mentions.users.first() || message.member || message.author;
 if(!user) return
 
 let e = new Discord.MessageEmbed()
-.setTitle(`Аватар`)
+.setTitle(language("AVATAR"))
 .setColor("#807fff")
 .setTimestamp()
 .setImage(user.displayAvatarURL({dynamic: true}))
-.setFooter(`Запросил ${message.author.username}`, message.author.displayAvatarURL())
+.setFooter(`${language("REQ")} ${message.author.username}`, message.author.displayAvatarURL())
 message.channel.send({embeds:[e]});
 }
 
