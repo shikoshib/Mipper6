5365ba50f14d028162ff221188f50b0fcc9c5131
shikoshib <shikoshib@yandex.ru>
2022-03-22 10:19:10 +1000

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,12 +1,17 @@
 exports.run = (client, message, args) => {
+    let guildLanguages = require("./guilds-language.json");
+    const guildLanguage = guildLanguages[message.guild.id] || "en"; 
+    const language = require(`./languages/${guildLanguage}`);
+  if (message.author.bot) return;
   const clientMember = message.guild.members.cache.get(client.user.id);
 if(!clientMember.permissions.has("BAN_MEMBERS")) return;
- if (!message.member.permissions.has('BAN_MEMBERS')) return message.channel.send('Вы на данный момент не имеете права делать это.')
+ if (!message.member.permissions.has('BAN_MEMBERS')) return message.reply(language("PERMS_LACK"))
 let id = message.content.split(" ").slice(1)
-if(!id[0]) return message.channel.send("Вы не указали ID забаненного участника.");
-if(isNaN(id[0])) return message.channel.send("Это не ID!")
+if(!id[0]) return message.reply(language("UNBAN_LACK"));
+if(isNaN(id[0])) return message.reply(language("IDISNAN"))
 message.guild.members.unban(id[0]);
-  message.channel.send(`<@${id}> был успешно разбанен.`)
+  message.channel.send(`<@${id}> ${language("UNBAN_SUCCESS")}`)
+
 }
 
 exports.name = "unban"
