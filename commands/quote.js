c01b6e013ad79a9f892db8e2fa3f82ef6580a702
shikoshib <shikoshib@yandex.ru>
2022-03-22 10:16:06 +1000

    Update quote.js

--- commands/quote.js
+++ commands/quote.js
@@ -1,46 +1,50 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+  let guildLanguages = require("./guilds-language.json");
+    const guildLanguage = guildLanguages[message.guild.id] || "en"; 
+    const language = require(`./languages/${guildLanguage}`);
+  
       var randomQ = Math.floor(Math.random() * 17) + 1;
 
         if (randomQ === 1) {
         message.channel.send('Lulumba 🙂')
         } else if (randomQ === 2) {
-        message.channel.send('Дорога шла вперёд, солнце светило, шкаф коричневый.')
+        message.channel.send(language("QUOTE_TWO"))
     } else if (randomQ === 3) {
-        message.channel.send('ъ')
+        message.channel.send(language("QUOTE_THREE"))
   } else if (randomQ === 4) {
-        message.channel.send('«Я такого не говорил» © Mipper6')
+        message.channel.send(language("QUOTE_FOUR"))
   } else if (randomQ === 5) {
-        message.channel.send('Интересный факт: если люди скажут, что они играют с читами, то все узнают, что они читеры.')
+        message.channel.send(language("QUOTE_FIVE"))
         } else if (randomQ === 6) {
-        message.channel.send('не обязан')
+        message.channel.send(language("QUOTE_SIX"))
   } else if (randomQ === 7) {
-        message.channel.send('RobTop ленивая срака')
+        message.channel.send(language("QUOTE_SEVEN"))
   } else if (randomQ === 8) {
-        message.channel.send('Кто как обзывается, тот сам так и называется')
+        message.channel.send(language("QUOTE_EIGHT"))
   } else if (randomQ === 9) {
           message.channel.send('what you know about rollin down in the deep')
   } else if (randomQ === 10) {
-          message.channel.send('но но но фир но лей')
+          message.channel.send(language("QUOTE_NINE"))
    } else if (randomQ === 11) {
           message.channel.send('أنا أحب ذلك عندما شعر الرجال الحصول على لطخت مع النفط')
    } else if (randomQ === 12) {
-          message.channel.send('если да, то это точно не нет')
+          message.channel.send(language("QUOTE_TEN"))
    } else if (randomQ === 13) {
-          message.channel.send('Плюсы и минусы болеть. Плюсы: ты болеешь. Минусы: ты болеешь')
+          message.channel.send(language("QUOTE_ELEVEN"))
    } else if (randomQ === 14) {
    const thonk = client.emojis.cache.get("913411955233017887");       
-          message.channel.send(`почему все хотят стать трусиками доры ${thonk}`)
+          message.channel.send(`${language("QUOTE_TWELVE")} ${thonk}`)
    } else if (randomQ === 15) {
-          message.channel.send("Короче пацаны рецепт оладушек. Кефир 2,5% - 500 мл. Мука - 3 стакана. Яйцо - 1 шт. Сахарок - 2 ст.л. Сода - 1 ч.л. Соль- ч.л.")
+          message.channel.send(language("QUOTE_FOURTEEN"))
    } else if (randomQ === 16) {
-          message.channel.send("Скажи клей") 
+          message.channel.send(language("QUOTE_FIFTEEN")) 
         } else if (randomQ === 17) {
           let rickroll = new Discord.MessageEmbed()
-          .setTitle("Отсканируй =)")
+          .setTitle(language("QUOTE_THIRTEEN"))
           .setImage("https://mipper6.cf/resources/qr%20kowka.gif")
           .setColor("#807fff")
-          .setFooter("пж")
+          .setFooter(language("QUOTE_THIRTEEN_FOOTER"))
           message.channel.send({embeds: [rickroll]})
         }
 }
