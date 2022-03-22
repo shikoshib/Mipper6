cdfbdbd9db82b372fe625960ef5576f7b02e641c
shikoshib <shikoshib@yandex.ru>
2022-03-22 10:07:58 +1000

    Update en.js

--- commands/languages/en.js
+++ commands/languages/en.js
@@ -105,7 +105,7 @@ const languageData = {
     QUOTE_FIFTEEN: "sussy baka",
     QUOTE_THIRTEEN_FOOTER: "pls",
     SETLANG_DESC: "to set the language",
-    EIGHTBALL_LACK: "You didn't specify the question.",
+    EIGHTBALL_LACK: "You didn't specify the question."
 };
 
 const translate = (key, ...args) => {
