2a18856699afee2a9035443b3ac89a3f0242de0f
shikoshib <shikoshib@yandex.ru>
2022-03-22 10:10:27 +1000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,18 +1,25 @@
+const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+    let guildLanguages = require("./guilds-language.json");
+    const guildLanguage = guildLanguages[message.guild.id] || "en"; 
+    const language = require(`./languages/${guildLanguage}`);
+  if (message.author.bot) return;
   const clientMember = message.guild.members.cache.get(client.user.id);
 if(!clientMember.permissions.has("BAN_MEMBERS")) return;
  const member = message.mentions.members.first()
- if (!member) return message.channel.send('Вы не указали, кого забанить.')
- if (!message.member.permissions.has('BAN_MEMBERS')) return message.channel.send('Вы на данный момент не имеете права делать это.')
- if (member.permissions.has('BAN_MEMBERS') || member.permissions.has('KICK_MEMBERS')) return message.channel.send('Участник, которого вы указали, является модератором (имеет право кикать или банить людей).')
+ if (!member) return message.reply(language("MOD_ERR"))
+ if (!message.member.permissions.has('BAN_MEMBERS')) return message.reply(language("PERMS_LACK"));
+  if (member.id === message.author.id) return message.reply(language("BAN_SELF"));
+  if (member.id === "929992928615608412") return message.reply(language("BOT_BAN")) 
+    if (member.permissions.has('BAN_MEMBERS') || member.permissions.has('KICK_MEMBERS')) return message.reply(language("KICK_PERMS_ERR"))
 
  try {
  member.ban().then(() => {
- message.channel.send(`${member} был успешно забанен.`)
+ message.channel.send(`${member} ${language("BAN_SUCCESS")}`)
  })
  } catch (err) {
  console.log(err)
- message.channel.send('Что-то пошло не так, повторите попытку.')
+ message.channel.send(language("BAN_ERR"))
  }
 }
 
