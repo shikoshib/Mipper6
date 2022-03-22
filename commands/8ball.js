7e982298e0db3f4005acfac90e654c55cd9a642b
shikoshib <shikoshib@yandex.ru>
2022-03-22 10:05:36 +1000

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,19 +1,22 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+    let guildLanguages = require("./guilds-language.json");
+    const guildLanguage = guildLanguages[message.guild.id] || "en"; 
+    const language = require(`./languages/${guildLanguage}`);
   let que = message.content.split(" ").slice(1).join(" ")
-  if(!que) return message.channel.send("Вы не указали вопрос.");
+  if(!que) return message.channel.send(language("EIGHTBALL_LACK"));
   var ans = [
-    'Да', 'Определённо', 'И не мечтай', 'Нет', '50/50', 'Возможно', 'Спроси ещё раз', 'Скорее, нет'
+    language("EIGHTBALL_ONE"), language("EIGHTBALL_TWO"), language("EIGHTBALL_THREE"), language("EIGHTBALL_FOUR"), language("EIGHTBALL_FIVE"), language("EIGHTBALL_SIX"), language("EIGHTBALL_SEVEN"), language("EIGHTBALL_EIGHT")
 ];
 
 var RandAns = ans[Math.floor(Math.random() * (ans.length))];
 let em = new Discord.MessageEmbed()
-  .setAuthor(`Запросил ${message.author.username}`, message.author.displayAvatarURL())
+  .setAuthor(`${language("REQ")} ${message.author.username}`, message.author.displayAvatarURL())
   .setColor("#807fff")
-  .setFooter("Mipper6, 2022. Все права отсутствуют.", "https://mipper6.cf/resources/Mipper6.png")
-  .addField("Ваш вопрос:", `${que}`)
-  .addField("Ответ:", `${RandAns}`)
+  .setFooter(`${language("RIGHTS")}`, "https://mipper6.cf/resources/Mipper6.png")
+  .addField(`${language("EIGHTBALL_Q")}`, `${que}`)
+  .addField(`${language("EIGHTBALL_A")}`, `${RandAns}`)
 message.channel.send({embeds:[em]})
-  }
+}
 
 exports.name = "8ball"
