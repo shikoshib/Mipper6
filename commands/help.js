501dbe89295917719145cd95b466aabebbd2f4f0
shikoshib <shikoshib@yandex.ru>
2022-03-22 10:12:44 +1000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,67 +1,72 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+    let guildLanguages = require("./guilds-language.json");
+    const guildLanguage = guildLanguages[message.guild.id] || "en"; 
+    const language = require(`./languages/${guildLanguage}`);
   if(message.content.toLowerCase() === "~help 1") {
     let embed1 = new Discord.MessageEmbed()
- .setTitle("🛡️ Модерация")
- .setAuthor("Все обновления на сайте!", "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
-  .addField("`~ban`", "забанить участника")
-  .addField("`~clear`", "очистить сообщения в чате")
-  .addField("`~kick`", "кикнуть участника")
-  .addField("`~unban`", "разбанить участника")
+ .setTitle(language("MOD_CATNAME"))
+ .setAuthor(language("WEBSITE"), "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
+  .addField("`~ban`", `${language("BAN_DESC")}`)
+  .addField("`~clear`", `${language("CLEAR_DESC")}`)
+  .addField("`~kick`", `${language("KICK_DESC")}`)
+  .addField("`~unban`", `${language("UNBAN_DESC")}`)
  .setColor("#807fff")
- .setFooter("Mipper6, 2022. Все права отсутствуют. | Текущая версия бота: 1.3", "https://mipper6.cf/resources/Mipper6.png")
+ .setFooter(`${language("RIGHTS")} | ${language("RELEASE")} ${language("REL_INDEX")}`, "https://mipper6.cf/resources/Mipper6.png")
     return message.channel.send({embeds:[embed1]});
   }
   
   if(message.content.toLowerCase() === "~help 2") {
     let embed2 = new Discord.MessageEmbed()
- .setTitle("📋 Информация")
- .setAuthor("Все обновления на сайте!", "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
-  .addField("`~botinfo`", "информация о боте")
-  .addField("`~ping`", "узнать задержку бота")
-  .addField("`~serverinfo`", "узнать информацию о сервере")
+.setTitle(language("INFO_CATNAME"))
+ .setAuthor(language("WEBSITE"), "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
+  .addField("`~botinfo`", language("BOTINFO_DESC"))
+  .addField("`~ping`", language("PING_DESC"))
+  .addField("`~serverinfo`", language("SERVERINFO_DESC"))
  .setColor("#807fff")
- .setFooter("Mipper6, 2022. Все права отсутствуют. | Текущая версия бота: 1.3", "https://mipper6.cf/resources/Mipper6.png")
+ .setFooter(`${language("RIGHTS")} | ${language("RELEASE")} ${language("REL_INDEX")}`, "https://mipper6.cf/resources/Mipper6.png")
     return message.channel.send({embeds:[embed2]});
   }
   
   if(message.content.toLowerCase() === "~help 3") {
     let embed3 = new Discord.MessageEmbed()
- .setTitle("😃 Развлечения")
- .setAuthor("Все обновления на сайте!", "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
-  .addField("`~8ball`", "шар судьбы")
-  .addField("`~quote`", "высказать «мудрую» фразу")
-  .addField("`~say`", "повторить сообщение от лица бота")
+.setTitle(language("FUN_CATNAME"))
+ .setAuthor(language("WEBSITE"), "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
+  .addField("`~8ball`", language("EIGHTBALL_DESC"))
+  .addField("`~quote`", language("QUOTE_DESC"))
+  .addField("`~say`", language("SAY_DESC"))
  .setColor("#807fff")
- .setFooter("Mipper6, 2022. Все права отсутствуют. | Текущая версия бота: 1.3", "https://mipper6.cf/resources/Mipper6.png")
+ .setFooter(`${language("RIGHTS")} | ${language("RELEASE")} ${language("REL_INDEX")}`, "https://mipper6.cf/resources/Mipper6.png")
     return message.channel.send({embeds:[embed3]});
   }
   
   if(message.content.toLowerCase() === "~help 4") {
     let embed4 = new Discord.MessageEmbed()
- .setTitle("⚙️ Прочее")
- .setAuthor("Все обновления на сайте!", "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
-  .addField("`~avatar`", "получить аватар участника")
-  .addField("`~lorem`", "написать текст Lorem Ipsum")
-  .addField("`~oir`", "подбросить монету")
-  .addField("`~rand1-10`", "получить случайное число от 1 до 10")
-  .addField("`~rand1-100`", "получить случайное число от 1 до 100")
+ .setTitle(language ("STH_CATNAME"))
+ .setAuthor(language("WEBSITE"), "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
+  .addField("`~avatar`", language("AVATAR_DESC"))
+  .addField("`~lorem`", language("LOREM_DESC"))
+  .addField("`~oir`", language("OIR_DESC"))
+  .addField("`~rand1-10`", language("RANDTEN_DESC"))
+  .addField("`~rand1-100`", language("RANDH_DESC"))
+  .addField("`~setlang`", language("SETLANG_DESC"))
  .setColor("#807fff")
- .setFooter("Mipper6, 2022. Все права отсутствуют. | Текущая версия бота: 1.3", "https://mipper6.cf/resources/Mipper6.png")
+ .setFooter(`${language("RIGHTS")} | ${language("RELEASE")} ${language("REL_INDEX")}`, "https://mipper6.cf/resources/Mipper6.png")
     return message.channel.send({embeds:[embed4]});
   }
   
+  const form = "`"
   let embed = new Discord.MessageEmbed()
-  .setTitle("Помощь по боту")
- .setAuthor("Все обновления на сайте!", "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
-  .setDescription("Это справка по всем командам бота.")
+  .setTitle(language ("GUIDE_NAME"))
+ .setAuthor(language("WEBSITE"), "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
+  .setDescription(language("HELP_DESC"))
   .setThumbnail("https://mipper6.cf/resources/Mipper6.png")
-  .addField("🛡️ Модерация:", "`~ban` `~clear` `~kick` `~unban` (подробнее: ~help 1)")
-  .addField("📋 Информация:", "`~botinfo` `~ping` `~serverinfo` (подробнее: ~help 2)")
-  .addField("😃 Развлечения:", "`~8ball` `~quote` `~say` (подробнее: ~help 3)")
-  .addField("⚙️ Прочее:", "`~avatar` `~lorem` `~oir` `~rand1-10` `~rand1-100` (подробнее: ~help 4)")
+  .addField(`${language("MOD_CATNAME")}:`, `${form}~ban${form} ${form}~clear${form} ${form}~kick${form} ${form}~unban${form} (${language("MORE")} ~help 1)`)
+  .addField(`${language("INFO_CATNAME")}:`, `${form}~botinfo${form} ${form}~ping${form} ${form}~serverinfo${form} (${language("MORE")} ~help 2)`)
+  .addField(`${language("FUN_CATNAME")}:`, `${form}~8ball${form} ${form}~quote${form} ${form}~say${form} (${language("MORE")} ~help 3)`)
+  .addField(`${language("STH_CATNAME")}:`, `${form}~avatar${form} ${form}~lorem${form} ${form}~oir${form} ${form}~rand1-10${form} ${form}~rand1-100${form} ${form}~setlang${form} (${language("MORE")} ~help 4)`)
   .setColor("#807fff")
-  .setFooter("Mipper6, 2022. Все права отсутствуют. | Текущая версия бота: 1.3", "https://mipper6.cf/resources/Mipper6.png")
+ .setFooter(`${language("RIGHTS")} | ${language("RELEASE")} ${language("REL_INDEX")}`, "https://mipper6.cf/resources/Mipper6.png")
 message.channel.send({embeds:[embed]})
 }
 
