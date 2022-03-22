9f9758d60cd80b26922a99027ffa324084201f93
shikoshib <shikoshib@yandex.ru>
2022-03-22 10:10:59 +1000

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,13 +1,18 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+    let guildLanguages = require("./guilds-language.json");
+    const guildLanguage = guildLanguages[message.guild.id] || "en"; 
+    const language = require(`./languages/${guildLanguage}`);
+    const form = "```"
 let botinfo = new Discord.MessageEmbed()
-    .setAuthor("Кликните здесь, чтобы добавить бота!", "https://freepngimg.com/thumb/arrow/4-2-arrow-free-download-png-thumb.png", "https://discord.com/oauth2/authorize?client_id=929992928615608412&permissions=2147483647&scope=bot%20applications.commands")
-    .setTitle("Информация о боте")
-    .setDescription("Вы сейчас просматриваете информацию о боте Mipper6.")
+    .setAuthor(language("INV_NAME"), "https://freepngimg.com/thumb/arrow/4-2-arrow-free-download-png-thumb.png", "https://discord.com/oauth2/authorize?client_id=929992928615608412&permissions=8&scope=bot%20applications.commands")
+    .setTitle(language("BOTINFO_TITLE"))
+    .setDescription(language("BOTINFO_CDESC"))
     .setThumbnail("https://mipper6.cf/resources/Mipper6.png")
-    .addField("Количество серверов с ботом", `${client.guilds.cache.size}`, true)
-    .addField("Текущая версия", "1.3", true)
-    .setFooter("Спасибо за использование бота! | Mipper6, 2022. Все права отсутствуют.", "https://mipper6.cf/resources/Mipper6.png")
+  .addField(language("BOT_OWNER"), `${form}shikoshib#9256${form}`)
+    .addField(language("SERV_NUM"), `${form}${client.guilds.cache.size}${form}`, true)
+    .addField(language("RELEASE"), `${form}${language("REL_INDEX")}${form}`)
+    .setFooter(`${language("THX")} | ${language("RIGHTS")}`, "https://mipper6.cf/resources/Mipper6.png")
     .setColor("#807fff")
       message.channel.send({embeds: [botinfo]})
 }
