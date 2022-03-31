3953adc5f21a73deee9f52451be99671e0a4bc68
shikoshib <shikoshib@yandex.ru>
2022-03-31 16:26:25 +1000

    Create stop.js

---
+++ commands/stop.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+const { Player, QueryType, getQueue } = require("discord-player");
+exports.run = async (client, message, args) => {
+    let guildLanguages = require("./guilds-language.json");
+    const guildLanguage = guildLanguages[message.guild.id] || "en"; 
+    const language = require(`./languages/${guildLanguage}`);
+  if (message.author.bot) return;
+  let queuelack = new MessageEmbed()
+  .setTitle(`❌ ${language("ERROR")}`)
+  .setDescription(`${language("QUEUE_LACK")}`)
+  .setColor("#807fff")
+
+  let success = new MessageEmbed()
+  .setTitle(`✅ ${language("STOP_TITLE")}`)
+  .setDescription(`${language("STOP_CDESC")}`)
+  .setColor("#807fff")
+  const queue = client.player.getQueue(message.guildId)
+  if(!queue) return await message.reply({embeds:[queuelack]})
+  
+  queue.destroy()
+  await message.reply({embeds:[success]})
+}
