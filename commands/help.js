a893db8e5cbf6cd3d203c122c049e4013ccd32b7
shikoshib <shikoshib@yandex.ru>
2022-03-31 16:22:00 +1000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,9 @@ exports.run = (client, message, args) => {
     let guildLanguages = require("./guilds-language.json");
     const guildLanguage = guildLanguages[message.guild.id] || "en"; 
     const language = require(`./languages/${guildLanguage}`);
+  const mp3player = client.emojis.cache.get("913735930102296576");
+  const wrench = client.emojis.cache.get("958622186913030194");
+  
   if(message.content.toLowerCase() === "~help 1") {
     let embed1 = new Discord.MessageEmbed()
  .setTitle(language("MOD_CATNAME"))
@@ -33,6 +36,8 @@ exports.run = (client, message, args) => {
 .setTitle(language("FUN_CATNAME"))
  .setAuthor(language("WEBSITE"), "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
   .addField("`~8ball`", language("EIGHTBALL_DESC"))
+  .addField("`~cat`", language("CAT_DESC"))
+  .addField("`~dog`", language("DOG_DESC"))
   .addField("`~quote`", language("QUOTE_DESC"))
   .addField("`~say`", language("SAY_DESC"))
  .setColor("#807fff")
@@ -42,7 +47,21 @@ exports.run = (client, message, args) => {
   
   if(message.content.toLowerCase() === "~help 4") {
     let embed4 = new Discord.MessageEmbed()
- .setTitle(language ("STH_CATNAME"))
+.setTitle(`${mp3player} ${language("MUS_CATNAME")}`)
+ .setAuthor(language("WEBSITE"), "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
+  .addField("`~pause`", language("PAUSE_DESC"))
+  .addField("`~play`", language("PLAY_DESC"))
+  .addField("`~resume`", language("RESUME_DESC"))
+  .addField("`~songinfo`", language("SONGINFO_DESC"))
+  .addField("`~stop`", language("STOP_DESC"))
+ .setColor("#807fff")
+ .setFooter(`${language("RIGHTS")} | ${language("RELEASE")} ${language("REL_INDEX")}`, "https://mipper6.cf/resources/Mipper6.png")
+    return message.channel.send({embeds:[embed4]});
+  }
+  
+  if(message.content.toLowerCase() === "~help 5") {
+    let embed5 = new Discord.MessageEmbed()
+ .setTitle(`${wrench} ${language("STH_CATNAME")}`)
  .setAuthor(language("WEBSITE"), "https://mipper6.cf/resources/Mipper6.png", "https://mipper6.cf/bot/updates")
   .addField("`~avatar`", language("AVATAR_DESC"))
   .addField("`~lorem`", language("LOREM_DESC"))
@@ -52,7 +71,7 @@ exports.run = (client, message, args) => {
   .addField("`~setlang`", language("SETLANG_DESC"))
  .setColor("#807fff")
  .setFooter(`${language("RIGHTS")} | ${language("RELEASE")} ${language("REL_INDEX")}`, "https://mipper6.cf/resources/Mipper6.png")
-    return message.channel.send({embeds:[embed4]});
+    return message.channel.send({embeds:[embed5]});
   }
   
   const form = "`"
@@ -63,8 +82,9 @@ exports.run = (client, message, args) => {
   .setThumbnail("https://mipper6.cf/resources/Mipper6.png")
   .addField(`${language("MOD_CATNAME")}:`, `${form}~ban${form} ${form}~clear${form} ${form}~kick${form} ${form}~unban${form} (${language("MORE")} ~help 1)`)
   .addField(`${language("INFO_CATNAME")}:`, `${form}~botinfo${form} ${form}~ping${form} ${form}~serverinfo${form} (${language("MORE")} ~help 2)`)
-  .addField(`${language("FUN_CATNAME")}:`, `${form}~8ball${form} ${form}~quote${form} ${form}~say${form} (${language("MORE")} ~help 3)`)
-  .addField(`${language("STH_CATNAME")}:`, `${form}~avatar${form} ${form}~lorem${form} ${form}~oir${form} ${form}~rand1-10${form} ${form}~rand1-100${form} ${form}~setlang${form} (${language("MORE")} ~help 4)`)
+  .addField(`${language("FUN_CATNAME")}:`, `${form}~8ball${form} ${form}~cat${form} ${form}~dog${form} ${form}~quote${form} ${form}~say${form} (${language("MORE")} ~help 3)`)
+  .addField(`${mp3player} ${language("MUS_CATNAME")}:`, `${form}~pause${form} ${form}~play${form} ${form}~resume${form} ${form}~songinfo${form} ${form}~stop${form} (${language("MORE")} ~help 4)`)
+  .addField(`${wrench} ${language("STH_CATNAME")}:`, `${form}~avatar${form} ${form}~lorem${form} ${form}~oir${form} ${form}~rand1-10${form} ${form}~rand1-100${form} ${form}~setlang${form} (${language("MORE")} ~help 5)`)
   .setColor("#807fff")
  .setFooter(`${language("RIGHTS")} | ${language("RELEASE")} ${language("REL_INDEX")}`, "https://mipper6.cf/resources/Mipper6.png")
 message.channel.send({embeds:[embed]})
