9e02d1963a6d4e56f53aa0e6044fbfac3572e9ed
shikoshib <shikoshib@yandex.ru>
2022-04-30 17:55:25 +1000

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,17 +1,26 @@
 const { MessageEmbed } = require("discord.js");
 const axios = require("axios");
 exports.run = async (client, message, args) => {
+  if(message.author.bot) return;
     let guildLanguages = require("./guilds-language.json");
     const guildLanguage = guildLanguages[message.guild.id] || "en"; 
     const language = require(`./languages/${guildLanguage}`);
-  axios
-  .get("https://aws.random.cat/meow")
-  .then(async (response) => {
-    const { data } = response;
+  axios.get('https://api.thecatapi.com/v1/images/search', { params: { limit:1, size:"full" } } ).then(async (response) => {
+    try {
+    this.image = response.data[0]
     let embed = new MessageEmbed()
-    .setImage(data.file)
+    .setImage(this.image.url)
     .setColor("#807fff")
     .setFooter(language("RIGHTS"), "https://mipper6.cf/resources/Mipper6.png")
     return message.channel.send({embeds:[embed]})
+    } catch (err) {
+    if (err.response) {
+        return message.reply(" 🚫 Something went wrong")
+    } else if (err.request) {
+        return message.reply(" 🚫 Something went wrong")
+    } else {
+        return message.reply(" 🚫 Something went wrong")
+    }
+}
   });
   }
