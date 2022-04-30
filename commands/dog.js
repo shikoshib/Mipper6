69ec3d808f2fbfbec6b6573d7d564d52a90b1c7f
shikoshib <shikoshib@yandex.ru>
2022-04-30 17:56:00 +1000

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 const axios = require("axios");
 exports.run = async (client, message, args) => {
+  if(message.author.bot) return;
     let guildLanguages = require("./guilds-language.json");
     const guildLanguage = guildLanguages[message.guild.id] || "en"; 
     const language = require(`./languages/${guildLanguage}`);
