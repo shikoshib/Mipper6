377b4e8c82edd6822de86b99e7eb8af14a4d2a10
shikoshib <shikoshib@yandex.ru>
2022-04-30 18:09:21 +1000

    Update pong.js

--- commands/pong.js
+++ commands/pong.js
@@ -1,4 +1,6 @@
+const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+  if(message.author.bot) return;
   message.channel.send("https://mipper6.cf/resources/duein-skala-johnson-i-chto-ty-tut-delaesh.gif")
 }
 
