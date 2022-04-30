2c2e3cbe0f8aa506c4e347926c653d7cd3486edb
shikoshib <shikoshib@yandex.ru>
2022-04-30 18:08:20 +1000

    Update oir.js

--- commands/oir.js
+++ commands/oir.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+  if(message.author.bot) return;
     let guildLanguages = require("./guilds-language.json");
     const guildLanguage = guildLanguages[message.guild.id] || "en"; 
     const language = require(`./languages/${guildLanguage}`);
