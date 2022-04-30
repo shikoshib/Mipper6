235392ccd45f9c5ce26c467be6fe09ccfde3114e
shikoshib <shikoshib@yandex.ru>
2022-04-30 17:52:16 +1000

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
+var os = require("os");
 exports.run = (client, message, args) => {
+  if(message.author.bot) return;
     let guildLanguages = require("./guilds-language.json");
     const guildLanguage = guildLanguages[message.guild.id] || "en"; 
     const language = require(`./languages/${guildLanguage}`);
@@ -12,6 +14,11 @@ totalSeconds %= 3600;
 let minutes = Math.floor(totalSeconds / 60);
 let seconds = Math.floor(totalSeconds % 60);
 let uptime = `\`\`\`${days} ${language("DAYS")}, ${hours} ${language("HOURS")}, ${minutes} ${language("MINUTES")} ${language("AND")} ${seconds} ${language("SECONDS")}\`\`\``;
+        const totalram = ((os.totalmem() / 10**6 + " ").split('.')[0]);
+        const freeram = ((os.freemem() / 10**6 + " ").split('.')[0]);
+        const usedram = (((os.totalmem() - os.freemem()) / 10**6 + " ").split('.')[0]);
+        const prctfreeram = (((os.freemem() * 100) / os.totalmem + " ").split('.')[0]);
+        const perc = 100 - prctfreeram;
 let botinfo = new Discord.MessageEmbed()
     .setAuthor(language("INV_NAME"), "https://freepngimg.com/thumb/arrow/4-2-arrow-free-download-png-thumb.png", "https://discord.com/oauth2/authorize?client_id=929992928615608412&permissions=8&scope=bot%20applications.commands")
     .setTitle(language("BOTINFO_TITLE"))
@@ -21,9 +28,10 @@ let botinfo = new Discord.MessageEmbed()
     .addField(language("SERV_NUM"), `${form}${client.guilds.cache.size}${form}`, true)
     .addField(language("RELEASE"), `${form}${language("REL_INDEX")}${form}`)
 .addField(language("UPTIME"), uptime)
+  .addField(language("RAM"), `${form}${language("TOTALRAM")}: ${Math.trunc(totalram / 1024)}${language("GB")} (${totalram} ${language("MB")})\n${language("USEDRAM")}: ${Math.trunc(usedram / 1024)}${language("GB")} (${usedram} ${language("MB")})\n${language("FREERAM")}: ${Math.trunc(freeram / 1024)}${language("GB")} (${freeram} ${language("MB")})\n${language("PERUSEDRAM")}: ${perc}%${form}`)
     .setFooter(`${language("THX")} | ${language("RIGHTS")}`, "https://mipper6.cf/resources/Mipper6.png")
     .setColor("#807fff")
       message.channel.send({embeds: [botinfo]})
-}
+          }
 
 exports.name = "botinfo"
