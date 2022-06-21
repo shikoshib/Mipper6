a0e6906c13758a79e318959b3048ef4278e92ce7
shikoshib <shikoshib@yandex.ru>
2022-06-21 15:11:29 +1000

    HAHAHAHAHA LMAO IT WAS BAN COMMAND

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,26 +1,27 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+  if(message.author.bot) return;
     let guildLanguages = require("./guilds-language.json");
     const guildLanguage = guildLanguages[message.guild.id] || "en"; 
     const language = require(`./languages/${guildLanguage}`);
-  if(message.author.bot) return;
-  const clientMember = message.guild.members.cache.get(client.user.id);
-if(!clientMember.permissions.has("BAN_MEMBERS")) return;
- const member = message.mentions.members.first()
- if (!member) return message.reply(language("MOD_ERR"))
- if (!message.member.permissions.has('BAN_MEMBERS')) return message.reply(language("PERMS_LACK"));
-  if (member.id === message.author.id) return message.reply(language("BAN_SELF"));
-  if (member.id === "929992928615608412") return message.reply(language("BOT_BAN")) 
-    if (member.permissions.has('BAN_MEMBERS') || member.permissions.has('KICK_MEMBERS')) return message.reply(language("KICK_PERMS_ERR"))
+  let user = message.mentions.users.first()
+  if(!user) {
+ user = message.author
+  }
+  let title = "Avatar"
+  if(guildLanguage === "ru") {
+  title = `Аватар ${user.tag}`
+  } else if(guildLanguage === "en") {
+  title = `${user.tag}'s avatar`
+  }
 
- try {
- member.ban().then(() => {
- message.channel.send(`${member} ${language("BAN_SUCCESS")}`)
- })
- } catch (err) {
- console.log(err)
- message.channel.send(language("BAN_ERR"))
- }
+let e = new Discord.MessageEmbed()
+.setTitle(title)
+.setColor("#807fff")
+.setTimestamp()
+.setImage(user.displayAvatarURL({format: "png", dynamic: true, size: 1024}))
+.setFooter(`${language("REQ")} ${message.author.username}`, message.author.displayAvatarURL())
+message.channel.send({embeds:[e]});
 }
 
-exports.name = "ban"
+exports.name = "avatar"
