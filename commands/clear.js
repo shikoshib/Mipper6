9eb494bca61719c3a196cecfae16279252385c7c
shikoshib <shikoshib@yandex.ru>
2022-06-21 15:14:15 +1000

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     let guildLanguages = require("./guilds-language.json");
     const guildLanguage = guildLanguages[message.guild.id] || "en"; 
     const language = require(`./languages/${guildLanguage}`);
-  if (message.author.bot) return;
+  if(message.author.bot) return;
   if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.reply(language("PERMS_LACK"));
    const clientMember = message.guild.members.cache.get(client.user.id);
 if(!clientMember.permissions.has("MANAGE_MESSAGES")) return;
